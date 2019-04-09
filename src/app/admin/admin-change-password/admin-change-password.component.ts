9bfcdd539bb1bdaf124f2f1b4c33b60308c43c40
akash1296 <1296.akash@gmail.com>
2019-04-09 21:46:46 +0530

    admin change password

--- src/app/admin/admin-change-password/admin-change-password.component.ts
+++ src/app/admin/admin-change-password/admin-change-password.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminChangePasswordService } from '../Services/admin-change-password.service';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,10 +9,10 @@ import { Router } from '@angular/router';
   styleUrls: ['./admin-change-password.component.scss']
 })
 export class AdminChangePasswordComponent implements OnInit {
-
+  ChangePasswordForm:FormGroup;
   public token
 
-  constructor(private ChangePasswordService:AdminChangePasswordService, private route:Router) {
+  constructor(private fb: FormBuilder,private ChangePasswordService:AdminChangePasswordService, private route:Router) {
      this.token=sessionStorage.getItem('tkn')
      if(this.token==""||!this.token||this.token==undefined||this.token==null){
        window.alert('YOU HAVE LOGGED OUT!! PLEASE LOGIN AGAIN');
@@ -20,22 +21,21 @@ export class AdminChangePasswordComponent implements OnInit {
 }
 
   ngOnInit() {
+    this.createForms();
   }
+
+  createForms(){
+    this.ChangePasswordForm = this.fb.group({
+      oldPassword:['',Validators.compose([Validators.required,]) ],
+      newPassword:['',Validators.compose([Validators.required,]) ],
+      confirmPassword:['',Validators.compose([Validators.required,]) ],
+  
+    })
+   }
+  
 
   onSubmitChangePasswordForm(value){
-    this.ChangePasswordService.changePassword(value).subscribe((res:any)=>{
-      if(res.success){
-        window.alert(res.msg);
-        this.route.navigate(['user']);
-      }else{
-        window.alert(res.msg);
-      }
-    })
+   console.log(value);
   }
   
-  logout(){
-    sessionStorage.removeItem('tkn')
-    this.route.navigate(['home'])
-  }
-
 }
